56e78fd6f1363729f8cfd95014308b6548d430a1
Eduardo <Eduardo@FSDFSDF5>
2017-04-16 14:55:49 -0300

    Revert "REMAPEO F.ARMIJO"
    This reverts commit 1dcc98d46dc0ae2a286558081c95bfe135e53fb3.

--- src/java/cl/cesfam/DAL/NewHibernateUtil.java
+++ src/java/cl/cesfam/DAL/NewHibernateUtil.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cl.cesfam.dal;
+package cl.cesfam.DAL;
 
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
@@ -12,9 +12,9 @@ import org.hibernate.SessionFactory;
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
  *
- * @author Francisco
+ * @author **Jorge Carrenca**
  */
-public class HibernateUtil {
+public class NewHibernateUtil {
 
     private static final SessionFactory sessionFactory;
     
